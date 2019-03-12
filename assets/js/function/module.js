10bf8aa5581e1058400c31c8db6e3ecdfb78811e
steking1207 <steking1207@gmail.com>
2019-03-13 00:23:56 +0800

    190313

---
+++ assets/js/function/module.js
@@ -0,0 +1,19 @@
+// Global
+$("#temp-top").load("module/temp.top.html");
+$("#temp-down").load("module/temp.down.html");
+$("#temp-customer").load("module/temp.customer.html");
+
+
+// Nav
+$("#nav-mobile").load("module/nav-mobile.html");
+
+// Inpage
+$("#temp-sleft").load("module/temp.sleft.html");
+
+// KV
+$("#temp-kv-about").load("module/temp.kv-about.html");
+$("#temp-kv-products").load("module/temp.kv-product.html");
+$("#temp-kv-customer").load("module/temp.kv-customer.html");
+$("#temp-kv-news").load("module/temp.kv-news.html");
+$("#temp-kv-contact").load("module/temp.kv-contact.html");
+$("#temp-kv-article").load("module/temp.kv-article.html");
